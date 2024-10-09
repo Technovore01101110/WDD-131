ea929c3439790227418c46a99aadf508de909b75
technovore01101110 <1sharpnathan@gmail.com>
2024-10-09 16:35:44 -0600

    modified:   coolpics/coolpics.css 	modified:   coolpics/coolpics.js 	modified:   coolpics/index.html 	new file:   debugging.js 	new file:   domEvents/domEvents.html 	new file:   domEvents/events.js 	new file:   javaDebug.html

---
+++ coolpics/coolpics.js
@@ -0,0 +1,43 @@
+const menuButton = document.querySelector(".menu-button");
+function toggleMenu() {
+    const menu = document.querySelector(".menu");
+    menu.classList.toggle("hide");
+
+}
+
+function handleResize(){
+    const menu = document.querySelector(".menu")
+    if (window.innerWidth > 1000){
+        menu.classList.remove("hide");
+    } else {
+        menu.classList.add("hide");
+ }
+}
+
+function viewerTemplate(pic, alt) {
+    return `<div class="viewer">
+        <button class="close-viewer">X</button>
+        <img src="${pic}" alt="${alt}">
+        </div>`;
+  }
+
+function closeViewer(){
+    console.log('function working')
+    const parent = event.target.closest('div');
+    parent.remove();
+}
+
+function viewHandler(){
+    const imgTarget = event.target;
+    const image = imgTarget.src.split('-');
+    const bigImage = image[0] + '-full.jpeg';
+    const html = viewerTemplate(bigImage);
+    document.body.insertAdjacentHTML("afterbegin", html);
+    document.querySelector('.close-viewer').addEventListener('click', closeViewer);
+}
+
+
+handleResize();
+menuButton.addEventListener('click', toggleMenu);
+window.addEventListener('resize', handleResize);
+document.querySelector('.gallery').addEventListener('click', viewHandler);
