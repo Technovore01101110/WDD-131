3dea1ec5e2244c593a006b70b5bbe548bf7e2b08
technovore01101110 <1sharpnathan@gmail.com>
2024-11-30 14:56:15 -0700

    deleted:    this-and-that-recipes/images/this and that recipes images/cafe-rio-chicken-burritos.jpg 	modified:   this-and-that-recipes/recipes.mjs

--- this-and-that-recipes/recipes.mjs
+++ this-and-that-recipes/recipes.mjs
@@ -2,18 +2,11 @@ const recipes = [
     {
         name: 'Beef Stroganoff',
         image: './images/this-and-that-recipes/beef-stroganoff.jpg',
-        tags: ['lunch', 'pasta'],
-        rating: null,
+        tags: ['main dish', 'pasta'],
+        rating: 7,
         time: null,
-        description: ''
-    },
-    {
-        name: 'Cafe Rio Chicken',
-        image: './images/this-and-that-recipes/cafe-rio-chicken-burritos.jpg',
-        tags: ['lunch', 'dinner'],
-        rating: null,
-        time: null,
-        description: ''
+        description: "I've tried this recipe a few times now, and this is how we like it. I cut this recipe in half for our little family, but use the full recipe when I have company. We like to eat ours with some tasty green beans on the side. It's a perfect, easy meal for a busy day. I do have one picky eater that won't eat it, but the rest of my kids like.",
+        instructions: '14 oz pasta noodles <br>1 lb. ground beef <br>1/ 2 tsp. garlic pwd. <br> 1/2 tsp. seasoning salt <br> 2-10oz can cream of mushroom soup <br>2 beef bouillon cube, dissolved in 1 c. warm water <br>2 t. worcestershire sauce <br>1/2 c. sour cream <br>Cook noodles according to package directions. In medium saucepan, cook hamburger until completely done, breaking meat in small pieces. Add garlic and seasoning salt. When cooked add soups and stir well. Add bouillon, Worcestershire sauce. Simmer until thickened. Add sour cream and stir till combined and heated through. When noodles are done, drain and add meat sauce. Serve warm.'
     },
     {
         name: 'Chicken Pesto Pasta',
