679f43d1e44ea373a7a3d6b7e7d740f88644cb9b
technovore01101110 <1sharpnathan@gmail.com>
2024-11-29 23:00:44 -0700

    Site plant 1.1

---
+++ this-and-that-recipes/recipes.mjs
@@ -0,0 +1,98 @@
+const recipes = [
+    {
+        name: 'Beef Stroganoff',
+        image: './images/this-and-that-recipes/beef-stroganoff.jpg',
+        tags: [lunch, pasta],
+        rating: null,
+        time: null,
+        description: ''
+    },
+    {
+        name: 'Cafe Rio Chicken',
+        image: './images/this-and-that-recipes/cafe-rio-chicken-burritos.jpg',
+        tags: [lunch, dinner],
+        rating: null,
+        time: null,
+        description: ''
+    },
+    {
+        name: 'Chicken Pesto Pasta',
+        image: './images/this-and-that-recipes/chicken-pesto-pasta.jpg',
+        tags: [lunch, pasta],
+        rating: null,
+        time: null,
+        description: ''
+    },
+    {
+        name: 'Chocolat Zucchini Bread',
+        image: './images/this-and-that-recipes/chocolate-zucchini-bread.jpg',
+        tags: [lunch, bread],
+        rating: null,
+        time: null,
+        description: ''
+    },  
+    {
+        name: 'Fruit Salad',
+        image: './images/this-and-that-recipes/fruit-salad.jpg',
+        tags: [lunch, dip, fruit],
+        rating: null,
+        time: null,
+        description: ''
+    },  
+    {
+        name: 'Grilled Chicken',
+        image: './images/this-and-that-recipes/grilled-chicken.jpg',
+        tags: [lunch, pasta],
+        rating: null,
+        time: null,
+        description: ''
+    },
+    {
+        name: 'Monster Cookies',
+        image: './images/this-and-that-recipes/monster-cookies.JPG',
+        tags: [dessert],
+        rating: null,
+        time: null,
+        description: ''
+    },
+    {
+        name: 'No-bake Cookies',
+        image: './images/this-and-that-recipes/no-bake-cookies.jpg',
+        tags: [lunch, pasta],
+        rating: null,
+        time: null,
+        description: ''
+    },
+    {
+        name: 'Pancakes',
+        image: './images/this-and-that-recipes/pancakes.jpg',
+        tags: ['breakfast'],
+        rating: null,
+        time: null,
+        description: ''
+    },
+    {
+        name: 'Sauted Zucchini',
+        image: './images/this-and-that-recipes/suate-zucchini.jpg',
+        tags: [side],
+        rating: null,
+        time: null,
+        description: ''
+    },
+    {
+        name: 'Tater Tot Casserole',
+        image: './images/this-and-that-recipes/tater-tot-casserole.jpg',
+        tags: [lunch, pasta],
+        rating: null,
+        time: null,
+        description: ''
+    },
+    {
+        name: 'Beef Stroganoff',
+        image: './images/this-and-that-recipes',
+        tags: [lunch, pasta],
+        rating: null,
+        time: null,
+        description: ''
+    }
+]
