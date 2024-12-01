87daa38bf9159f919c0f1c980bf4d82b07be5a06
technovore01101110 <1sharpnathan@gmail.com>
2024-11-30 18:44:15 -0700

    finished site plan

--- this-and-that-recipes/recipes.mjs
+++ this-and-that-recipes/recipes.mjs
@@ -3,7 +3,7 @@ const recipes = [
         name: 'Beef Stroganoff',
         image: './images/this-and-that-recipes/beef-stroganoff.jpg',
         tags: ['main dish', 'pasta', 'beef'],
-        rating: 7,
+        rating: 4,
         time: null,
         description: "I've tried this recipe a few times now, and this is how we like it. I cut this recipe in half for our little family, but use the full recipe when I have company. We like to eat ours with some tasty green beans on the side. It's a perfect, easy meal for a busy day. I do have one picky eater that won't eat it, but the rest of my kids like.",
         instructions: '14 oz pasta noodles <br>1 lb. ground beef <br>1/ 2 tsp. garlic pwd. <br> 1/2 tsp. seasoning salt <br> 2-10oz can cream of mushroom soup <br>2 beef bouillon cube, dissolved in 1 c. warm water <br>2 t. worcestershire sauce <br>1/2 c. sour cream <br>Cook noodles according to package directions. In medium saucepan, cook hamburger until completely done, breaking meat in small pieces. Add garlic and seasoning salt. When cooked add soups and stir well. Add bouillon, Worcestershire sauce. Simmer until thickened. Add sour cream and stir till combined and heated through. When noodles are done, drain and add meat sauce. Serve warm.'
@@ -25,16 +25,7 @@ const recipes = [
         time: null,
         description: 'Super rich and chocolatey.  My kids were super excited about this treat.  I didn\'t mention it had zucchini...no one needed to know :) ',
         instructions: '2 1/2 cups all-purpose flour <br>1/2 cup cocoa <br>1 teaspoon baking soda <br>1/2 teaspoon baking powder <br>1 teaspoon salt <br>1 1/2 cups sugar <br>3 eggs1 cup canola oil (or apple sauce) <br>2 teaspoons vanilla <br>2 packed cups grated zucchini (don\’t peel it) <br>1 cup chocolate chips (I used mini) <br><br>Prehat oven at 350 F. Spray two 9×5 inch pans with nonstick spray. In a large bowl, stir together flour, cocoa, baking soda, baking powder and salt. In a medium bowl, beat the sugar and eggs until light and fluffy. Add the oil and vanilla. Add to the dry ingredients along with the zucchini and chocolate chips. Stir just until combined, and divide between the loaf pans. Bake for 50-60 minutes, or until the tops are cracked and springy to the touch. Makes 2 cakes.'
-    },  
-    {
-        name: 'Fruit Salad',
-        image: './images/this-and-that-recipes/fruit-salad.jpg',
-        tags: ['side', 'dip', 'fruit'],
-        rating: 4,
-        time: null,
-        description: '',
-        instructions: ''
-    },  
+    },   
     {
         name: 'Air Fried Chicken',
         image: './images/this-and-that-recipes/grilled-chicken.jpg',
@@ -77,8 +68,8 @@ const recipes = [
         tags: ['main dish', 'pasta', 'grean beans'],
         rating: 4,
         time: null,
-        description: '',
-        instructions: ''
+        description: 'A delicous combo of ground beef and vegetables preserved from the garden, making it a healthy and tasty meal.',
+        instructions: '1 medium onion <br>1 lb ground beef <br>3 cups green beans, chopped <br>2 cups frozen corn <br>¼ cup butter <br>¼ cup flour <br>1 cup milk <br>1 heaping Tablespoon beef bullion <br>Salt and pepper to taste <br>Large bag of tater tots <br>½ cups cheddar cheese (I used a Mexican blend)<br><br>Dice onion and cook with ground beef until fully cooked. Meanwhile, cook your green beans and corn until tender. Melt butter in the pan with the hamburger.  Stir in flour to the melted butter and meat mixture.  Stir until combined.  Quickly stir in the milk.  Add beef bullion and bring to a simmer. Drain vegetables and add them to the meat and sauce mixture. Add meat and vegetable mixture to a greased 9x13 pan. Top the mixture with a layer of tater tots. Bake in a preheated oven at 375 for about 30 minutes or until the tater tots are crisp and golden.  Sprinkle tater tots with a layer of cheddar cheese.  Bake an additional 3-5 minutes or until the cheese is melted. Serve warm.'
     },
     {
         name: 'Strawberry Cream Cheese Wontons',
@@ -96,7 +87,7 @@ const recipes = [
         rating: 4,
         time: null,
         description: '',
-        instructions: ''
+        instructions: '6 eggs <br> 1/2 cup of butter <br>1 cup of flour <br>1 cup of milk <br>1/2 teaspoon <br><br>Heat oven to 450. Melt 1/4 cup butter in 9x13 pan. Blend in VitaMix (or blender) until well mixed. Spread butter around pan and pour batter in. Cook 15 to 20 minutes. Serve Warm.'
     },
     {
         name: 'Cheesey Green Bean Casserole',
